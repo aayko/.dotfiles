2f4951ba777109f1d467620cf0948b5eeb1449e0
aayko <maxglt2001@gmail.com>
2023-08-18 00:06:33 +0200

    keybinds cleaning

--- .config/nvim/lua/keymaps.lua
+++ .config/nvim/lua/keymaps.lua
@@ -7,17 +7,6 @@ keymap.set("n", "<leader>r", [[:%s/\<<C-r><C-w>\>/<C-r><C-w>/gI<Left><Left><Left
 keymap.set('n', 'k', "v:count == 0 ? 'gk' : 'k'", { expr = true })
 keymap.set('n', 'j', "v:count == 0 ? 'gj' : 'j'", { expr = true })
 
--- Move lines with alt+hjkl
-keymap.set('n', '<A-j>', ':m .+1<CR>==', { silent = true })
-keymap.set('n', '<A-k>', ':m .-2<CR>==', { silent = true })
-keymap.set('i', '<A-j>', '<Esc>:m .+1<CR>==gi', { silent = true })
-keymap.set('i', '<A-k>', '<Esc>:m .-2<CR>==gi', { silent = true })
-keymap.set('v', '<A-j>', ":m '>+1<CR>gv=gv", { silent = true })
-keymap.set('v', '<A-k>', ":m '<-2<CR>gv=gv", { silent = true })
-
--- New file
-keymap.set("n", "<leader>fn", vim.cmd.enew, { desc = "New File" })
-
 -- Save file
 keymap.set({ "i", "v", "n", "s" }, "<C-s>", "<CMD>update<CR><ESC>", { desc = "Save file" })
 
@@ -44,24 +33,14 @@ keymap.set("n", "<leader>lw", "<CMD>set wrap!<CR>", { silent = true })
 -- Unbind space outside of insert
 keymap.set({ 'n', 'v' }, '<space>', '<nop>')
 
--- Delete a word using Ctrl+Backspace
-keymap.set("i", "<C-BS>", "<C-w>")
-keymap.set("c", "<C-BS>", "<C-w>")
-keymap.set("i", "<C-H>", "<C-w>")
-keymap.set("c", "<C-H>", "<C-w>")
-
--- Go to start-of-line/end-of-line
-keymap.set({ "n", "v" }, "0", "^")
-keymap.set({ "n", "v" }, "^", "0")
-
 -- Move to window using the movement keys
 keymap.set("n", "<left>", "<C-w>h")
 keymap.set("n", "<down>", "<C-w>j")
 keymap.set("n", "<up>", "<C-w>k")
 keymap.set("n", "<right>", "<C-w>l")
 
-keymap.set("n", "L", "$")
-keymap.set("n", "H", "^")
+keymap.set({ "n", "v" }, "L", "$")
+keymap.set({ "n", "v" }, "H", "^")
 
 keymap.set({ "n", "v" }, "s", "V")
 
@@ -86,6 +65,7 @@ keymap.set("n", "<C-i>", "<C-i>zz", { noremap = true })
 -- Center on first search
 vim.cmd("cnoremap <silent><expr> <enter> index(['/', '?'], getcmdtype()) >= 0 ? '<enter>zz' : '<enter>'")
 
+-- Resize windows
 keymap.set("n", "<C-Up>", "<CMD>resize +2<CR>", { desc = "Increase window height", silent = true })
 keymap.set("n", "<C-Down>", "<CMD>resize -2<CR>", { desc = "Decrease window height", silent = true})
 keymap.set("n", "<C-Left>", "<CMD>vertical resize -2<CR>", { desc = "Decrease window width", silent = true})
@@ -93,9 +73,6 @@ keymap.set("n", "<C-Right>", "<CMD>vertical resize +2<CR>", { desc = "Increase w
 
 -- Paste over while keeping register
 keymap.set("v", "<leader>p", "\"_dP")
-
--- Themery
-keymap.set("n", "<leader>th", "<CMD>Themery<CR>")
 
 -- NvimTree
 keymap.set('n', '<leader>e', vim.cmd.NvimTreeToggle)
