b954e868efae491f3976ed1445aa94aeddf41329
aayko <aayko.dev@pm.me>
2023-09-20 20:25:33 +0200

    rm, tree, codium aliases

--- .config/zsh/aliases.zsh
+++ .config/zsh/aliases.zsh
@@ -2,11 +2,12 @@ alias dot="/usr/bin/git --git-dir=$HOME/.dotfiles/ --work-tree=$HOME"
 alias lf="lfcd"
 
 alias e="nvim"
+alias code="codium"
 alias se="sudoedit"
 alias epk="nvim ~/packages-list"
 
 alias mkdir="mkdir -vp"
-alias rmv="rm -v"
+alias rm="rm -v"
 alias cp="cp -v"
 alias mv="mv -v"
 
@@ -16,7 +17,7 @@ alias free='free -m'
 alias grep='grep --color=always'
 alias ls="lsd -A --group-directories-first --sort=extension"
 alias ll="lsd -Al --group-directories-first --sort=extension"
-alias tree="lsd -A --depth=2 --tree --group-directories-first --sort=extension"
+alias tree="lsd -A --depth=4 --tree --group-directories-first --sort=extension"
 alias cat="bat"
 
 alias p="sudo pacman"
