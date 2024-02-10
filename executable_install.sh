64491642a851e230919ed984dbdbace491297228
aayko <aayko.dev@pm.me>
2024-02-10 19:45:24 +0100

    Update install.sh

--- executable_install.sh
+++ executable_install.sh
@@ -89,8 +89,20 @@ ask "Install haskell stuff?" && {
 
 ask "Install zap plugin manager?" && zsh <(curl -s https://raw.githubusercontent.com/zap-zsh/zap/master/install.zsh) --keep --branch release-v1 && echo "zap installed"
 
-ask "Install chezmoi" && {
-    chezmoi init https://github.com/aayko/.dotfiles &&
+ask "Setup chezmoi" && {
+    wget -O- "https://raw.githubusercontent.com/aayko/.dotfiles/main/private_dot_ssh/encrypted_id_rsa.pub.age" | age --decrypt --identity /home/ayko/.age/key > /home/ayko/.ssh/id_rsa.pub &&
+    wget -O- "https://raw.githubusercontent.com/aayko/.dotfiles/main/private_dot_ssh/encrypted_private_id_rsa.age" | age --decrypt --identity /home/ayko/.age/key > /home/ayko/.ssh/id_rsa &&
+    mkdir -p /home/"$USER"/.config/chezmoi &&
+	cat > /home/ayko/.config/chezmoi/chezmoi.toml <<-EOF
+	encryption = "age"
+	[age]
+	    identity = "/home/ayko/.age/key"
+	    symmetric = true
+	[git]
+	    autoCommit = true
+	    autoPush = true
+	EOF
+    chezmoi init git@github.com:aayko/.dotfiles &&
     chezmoi update
 }
 
