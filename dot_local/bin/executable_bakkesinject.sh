ec13e6d797284d192cb8770fe4c53bdc9de4ad20
aayko <aayko.dev@pm.me>
2024-02-19 18:08:44 +0100

    Add .local/bin/bakkesinject.sh

---
+++ dot_local/bin/executable_bakkesinject.sh
@@ -0,0 +1,9 @@
+echo "" > ~/steam-252950.log
+
+eval 'PROTON_LOG=1 "$@"' &
+
+while ! grep "Initializing Engine Completed" ~/steam-252950.log > /dev/null; do
+    sleep 1
+done
+
+protontricks -c 'wine ~/.local/bin/inject.exe' 252950
