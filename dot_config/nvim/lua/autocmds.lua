3c50fad92baf69444c7ba6f2457a618956047399
aayko <aayko.dev@pm.me>
2024-02-22 18:45:17 +0100

    Update .config/lf/lfrc Update .config/nvim/lua/autocmds.lua

--- dot_config/nvim/lua/autocmds.lua
+++ dot_config/nvim/lua/autocmds.lua
@@ -26,6 +26,7 @@ vim.api.nvim_create_autocmd("BufReadPost", {
   end,
 })
 
+vim.cmd([[ autocmd FileType nix autocmd VimEnter * TSContextDisable ]])
 vim.cmd([[ autocmd FileType tex autocmd BufWritePost <buffer> silent make ]])
 vim.cmd([[ autocmd FileType php hi! link @Variable GruvboxBlue ]])
 vim.cmd([[ autocmd VimEnter * if luaeval('require("oil").get_current_dir()') == '/home/ayko/notes/' | call luaeval('require("oil").toggle_hidden()') | endif ]])
