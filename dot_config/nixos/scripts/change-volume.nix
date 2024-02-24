043c30a1c250aba02ffbcd08a0e6cbb8b8c576af
aayko <aayko.dev@pm.me>
2024-02-25 00:27:24 +0100

    Update .config/nixos/scripts/change-volume.nix Update .local/bin/change-volume

--- dot_config/nixos/scripts/change-volume.nix
+++ dot_config/nixos/scripts/change-volume.nix
@@ -24,25 +24,18 @@ pkgs.writeShellApplication {
 
     case $1 in
     up)
-        pamixer -u
         pamixer -i 5
         send_notification
         ;;
     down)
         pamixer -d 5
-        if [[ $(pamixer --get-volume) -lt 6 ]]; then
-            pamixer -m
-            send_muted
-        else
-            send_notification
-        fi
+        send_notification
         ;;
     mute)
         pamixer -t
         if [[ $(pamixer --get-mute) == "true" ]]; then
             send_muted
         else
-            pamixer --set-volume 5
             send_notification
         fi
         ;;
