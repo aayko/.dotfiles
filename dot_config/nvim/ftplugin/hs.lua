0a39fe3de8b426a0dc42963a292bbc5f9a6383da
aayko <aayko.dev@pm.me>
2024-03-02 18:10:46 +0100

    Add .config/nvim/ftplugin/hs.lua

---
+++ dot_config/nvim/ftplugin/hs.lua
@@ -0,0 +1,4 @@
+local buf = vim.api.nvim_get_current_buf()
+vim.api.nvim_buf_set_option(buf, 'tabstop', 2)
+vim.api.nvim_buf_set_option(buf, 'softtabstop', 2)
+vim.api.nvim_buf_set_option(buf, 'shiftwidth', 2)
