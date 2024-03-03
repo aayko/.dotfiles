d211e0e29654a7006a2eb7478137493a03ad3a6b
aayko <aayko.dev@pm.me>
2024-03-03 23:08:16 +0100

    Update .config/nixos/host/desktop/hardware-configuration.nix

--- dot_config/nixos/host/desktop/hardware-configuration.nix
+++ dot_config/nixos/host/desktop/hardware-configuration.nix
@@ -37,5 +37,6 @@
   # networking.interfaces.wlp4s0.useDHCP = lib.mkDefault true;
 
   nixpkgs.hostPlatform = lib.mkDefault "x86_64-linux";
+  hardware.enableAllFirmware = true;
   hardware.cpu.intel.updateMicrocode = lib.mkDefault config.hardware.enableRedistributableFirmware;
 }
