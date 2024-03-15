a44cef7ad9f71e7029cb0669cef724967aa02227
aayko <aayko.dev@pm.me>
2024-03-15 13:33:54 +0100

    Update .config/nix/flake.lock Update .config/nix/flake.nix Update .config/nix/modules/system/nixconf.nix

--- dot_config/nix/modules/system/nixconf.nix
+++ dot_config/nix/modules/system/nixconf.nix
@@ -1,3 +1,5 @@
+{ inputs, ... }:
+
 {
   nix = {
     settings = {
@@ -8,15 +10,15 @@
     gc.dates = "19:00";
   };
 
-  nixpkgs.config = {
-    allowUnfree = true;
-    packageOverrides = pkgs: {
-      stable = import
-        (fetchTarball {
-          url = "https://github.com/NixOS/nixpkgs/archive/nixos-23.11.tar.gz";
-          sha256 = "sha256:1gvricg9hinsc399d76im9vn3wjn34wcmfzfflwyz9yhr5npcr4b";
-        })
-        { };
-    };
+  nixpkgs = {
+    overlays = [
+      (final: _prev: {
+        stable = import inputs.nixpkgs-stable {
+          system = final.system;
+          config.allowUnfree = true;
+        };
+      })
+    ];
+    config.allowUnfree = true;
   };
 }
