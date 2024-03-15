0948aeee31db636df05819c54c16b48150ba962b
aayko <aayko.dev@pm.me>
2024-03-15 17:27:54 +0100

    Update .config/nix/modules/services/default.nix

--- dot_config/nix/modules/services/default.nix
+++ dot_config/nix/modules/services/default.nix
@@ -6,7 +6,6 @@
 
   services.openssh.enable = true;
   services.dbus.enable = true;
-  services.sshd.enable = true;
   services.printing.enable = true;
   services.udisks2.enable = true;
   services.gvfs.enable = true;
