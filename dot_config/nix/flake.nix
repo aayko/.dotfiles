b41a1a05ae33d23d7e893fba84a3323f42b40339
aayko <aayko.dev@pm.me>
2024-03-16 17:36:20 +0000

    Update .config/nix/flake.lock Update .config/nix/flake.nix Update .config/nix/hosts/desktop/configuration.nix Update .config/nix/hosts/laptop/configuration.nix

--- dot_config/nix/flake.nix
+++ dot_config/nix/flake.nix
@@ -4,6 +4,10 @@
   inputs = {
     nixpkgs.url = "github:nixos/nixpkgs/nixos-unstable";
     nixpkgs-stable.url = "github:nixos/nixpkgs/nixos-23.11";
+    nixos-wsl = {
+      url = "github:nix-community/NixOS-WSL";
+      inputs.nixpkgs.follows = "nixpkgs";
+    };
 
     # home-manager = {
     #   url = "github:nix-community/home-manager";
@@ -29,6 +33,13 @@
           specialArgs = { inherit inputs; };
           modules = [
             ./hosts/laptop/configuration.nix
+            # inputs.home-manager.nixosModules.default
+          ];
+        };
+        kumo = nixpkgs.lib.nixosSystem {
+          specialArgs = { inherit inputs; };
+          modules = [
+            ./hosts/wsl/configuration.nix
             # inputs.home-manager.nixosModules.default
           ];
         };
