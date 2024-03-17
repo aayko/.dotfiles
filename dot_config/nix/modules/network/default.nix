4e0e755a2c3d0838c5a368870e26ced1d128268e
aayko <aayko.dev@pm.me>
2024-03-17 10:58:44 +0100

    Add .config/nix/modules/network/default.nix

---
+++ dot_config/nix/modules/network/default.nix
@@ -0,0 +1,13 @@
+{
+  imports = [
+    ./resolved.nix
+  ];
+
+  # Configure network proxy if necessary
+  # networking.proxy.default = "http://user:password@proxy:port/";
+  # networking.proxy.noProxy = "127.0.0.1,localhost,internal.domain";
+  networking.networkmanager.enable = true;
+
+  # Or disable the firewall altogether.
+  networking.firewall.enable = true;
+}
